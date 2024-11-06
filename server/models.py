0c2495df5817a40402c67333b8972ee2cb3590bf
Dylan Tin <dylant711@gmail.com>
2024-11-06 14:12:17 -0500

    created 1st model

--- server/models.py
+++ server/models.py
@@ -4,3 +4,14 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+
+class BubbleTea(db.Model, SerializerMixin):
+    __tablename__ = "bubble_tea"
+
+    id = db.Column(db.Integer, primary_key=True)
+    franchise = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    topping = db.Column(db.String, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    
