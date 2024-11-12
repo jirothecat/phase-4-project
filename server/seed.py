97c45c37b99bfc52a9c4cab5f747c867e518cc64
Dylan Tin <dylant711@gmail.com>
2024-11-11 19:37:28 -0500

    initial commit

--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,19 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, BubbleTea
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+
+        BubbleTea.query.delete()
+
+        bubble_tea_1 = BubbleTea(franchise="Molly Tea", image="https://s3-media0.fl.yelpcdn.com/bphoto/pqQdxJYmf2l9hIeEXaHVCQ/348s.jpg", price="$6.79", topping="Tapioca", location="Flushing")
+
+        db.session.add_all([bubble_tea_1])
+        db.session.commit()
+
+        print("Seeded database.")
